d5badc024f71a00c3e76dd2863a37beaf7906573
KMoore7 <carla.moore3@icloud.com>
2022-08-25 18:12:15 -0400

    this is the to lower case method that will turn all of the letters in a string to lower case.

---
+++ toLowerCase.js
@@ -0,0 +1,6 @@
+// .toLowerCase() — Lower cases entire string
+// The toLowerCase() function returns a string with all lower case letters.
+
+let str = 'This has BIG LETTERS. They want to all be small.';
+
+console.log(str.toLowerCase()); // "this has big letters. they want all to be small."
