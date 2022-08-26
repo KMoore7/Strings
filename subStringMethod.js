580245386a0d9fde65be10c838cc2f46e9cb2daa
KMoore7 <carla.moore3@icloud.com>
2022-08-26 17:35:44 -0400

    This is an example of how to use the subString method to return a substring from a string of characters

---
+++ subStringMethod.js
@@ -0,0 +1,6 @@
+//string.substring(a,b)
+
+const name = "Karla Moore"; 
+console.log(name.substring(0,5)) //will return Karla
+console.log(name.substring(6,11)) //Moore
+console.log(name.substring(0,2)) //Ka
