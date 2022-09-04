81fb76699e35f99465c608155ad1bcad553fbcbd
KMoore7 <carla.moore3@icloud.com>
2022-09-03 21:24:42 -0400

    This is how you determine how long a string is with the .length method

---
+++ lengthMethod.js
@@ -0,0 +1,4 @@
+// Calling .length on a string will return the number of character that the string contains.
+
+let stringLength = 'This string has a lot of characters.'
+console.log(stringLength.length) //This will return 35 because there are 35 characters in this string.
